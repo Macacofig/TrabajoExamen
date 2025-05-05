2b7c93113f5020f4854b20ecfa187d0ae400618a
Camila <camila.carvajal@ucb.edu.bo>
2025-05-05 17:37:30 -0400

    estilos de la calculadora

--- Calculadora.js
+++ Calculadora.js
@@ -1,11 +1,16 @@
-document.getElementById("buttonEnter").addEventListener("click", function() {
+document.getElementById("buttonEnter").addEventListener("click", function () {
     calculateIMC();
-        });
+});
+
 function calculateIMC() {
     var weight = parseFloat(document.getElementById("weight").value);
     var length = parseFloat(document.getElementById("length").value);
-    var imc =parseFloat( weight / (length * length));
-    document.getElementById("result").innerHTML = `Tu IMC es de: ${imc.toFixed(2)}<br>`;
 
-    
-}   
+    if (!weight || !length) {
+        document.getElementById("result").innerHTML = "Por favor, introduce peso y altura válidos.";
+        return;
+    }
+
+    var imc = weight / (length * length);
+    document.getElementById("result").innerHTML =`Tu IMC es de: ${imc.toFixed(2)}   ` ;
+}
